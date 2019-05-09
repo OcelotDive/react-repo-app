7aea9586956dea8605557563e8d6cdc6bc619cd7
ocelotdive <oscar1dlh@hotmail.com>
2019-05-09 13:08:38 +0100

    403 error

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,57 @@
 const React = require("react");
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require("react-router-dom");
 
 class Results extends React.Component {
-    
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            winner: null,
+            loser: null,
+            error: null,
+            loading: true
+        }
+    }
+    componentDidMount() {
+         const players = queryString.parse(this.props.location.search);
+        api.battle([
+            players.playerOneName,
+            players.playerTwoName
+        ]).then(function(players) {
+            
+            if(players === null) { //if error
+                return this.setState(()=> {
+                    return {
+                        error: "Looks like an error occurred",
+                        loading: false
+                    }
+                })
+            }
+            //if no error
+            this.setState(()=> {
+                return {
+                    error: null,
+                    winner: results[0],
+                    loser: results[1],
+                    loading: false
+                }
+            });
+        }.bind(this))
+    }
+                
     render() {
     //will show props passed by react router    console.log(this.props)
-        return(
-            <div>Results</div>
+       
+        const error = this.state.error;
+        const winner = this.state.winner;
+        const loser = this.state.loser;
+        const loading = this.state.loading;
         
-        )
+      return (
+        <div>Results</div>
+      )
     }
 }
 
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,74 @@
 const axios = require('axios');
+// these are mini functions that comprise the main getUserData function.
+function getProfile(username) {
+    return axios.get("https://api.github.com/users/" + username)
+         .then(function(user) {
+        return user.data;
+});
+}
 
-
-module.exports = {
+/*getProfile('tylermcginnis')
+    .then(function(data) {
     
+});*/
+
+function gitRepos(username) {
+ 
+    return axios.get("https://api.github.com/users/" + username + "/repos" + "&per_page=100")
+             
+
+}
+
+
+function getStarCount(repos) {
+    return repos.data.reduce((count, repo) => {
+        return count + repo.stargazers_count;
+    })
+}
+
+function calculateScore(profile, repos) {
+    const followers = profile.followers;
+    const totalStars = getStarCount(repos);
+    
+    return (followers * 3) + totalStars;
+}
+
+function handleError (error) {
+    console.warn(error);
+    return null;
+    
+}
+//axios.all returns an array of promises
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        gitRepos(player)
+    ]).then((data) => {
+        console.log(data)
+        const profile = data[0];
+        const repos = data[1];
+        
+        return {
+            profile: profile, 
+            score: calculateScore(profile, repos)
+        }
+        
+    })
+}
+
+function sortPlayers(players) {
+    return players.sort((a,b) => {
+        return b.score - a.score;
+    })
+}
+//players.map will return a new array with each index containing an object returned //from the
+//getUserData function which is just the same as passing an inline callback to .map
+module.exports = {
+    battle: function (players) {
+      return axios.all(players.map(getUserData))
+        .then(sortPlayers)
+        .catch(handleError)
+    },
     fetchPopularRepos: function(lang) {
         const encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language: '+ lang + '&sort=stars&order=desc&type=Repositories');
         
